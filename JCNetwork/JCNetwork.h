67ebf5cd486fdf9198a458b7c674dbb22db65878
Jam <jam@192.168.0.109>
2013-06-23 10:25:11 +0800

    Initial Commit

---
+++ JCNetwork/JCNetwork.h
@@ -0,0 +1,13 @@
+//
+//  JCNetwork.h
+//  JCNetwork
+//
+//  Created by Jam on 13-6-23.
+//  Copyright (c) 2013年 Jam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JCNetwork : NSObject
+
+@end
---
+++ JCNetwork/JCNetwork.m
@@ -0,0 +1,13 @@
+//
+//  JCNetwork.m
+//  JCNetwork
+//
+//  Created by Jam on 13-6-23.
+//  Copyright (c) 2013年 Jam. All rights reserved.
+//
+
+#import "JCNetwork.h"
+
+@implementation JCNetwork
+
+@end
