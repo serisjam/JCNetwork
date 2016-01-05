865037c884bf979706f5258104f18be5d03bfda0
Jam <jiamiao@mayiy.com>
2016-01-05 16:26:52 +0800

    Initial Commit

---
+++ JCNetwork/JCNetwork.h
@@ -0,0 +1,19 @@
+//
+//  JCNetwork.h
+//  JCNetwork
+//
+//  Created by Jam on 16/1/5.
+//  Copyright © 2016年 Jam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JCNetwork.
+FOUNDATION_EXPORT double JCNetworkVersionNumber;
+
+//! Project version string for JCNetwork.
+FOUNDATION_EXPORT const unsigned char JCNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JCNetwork/PublicHeader.h>
+
+
