dbdc86dd41102af24cea1958d2f12bdb3e1b1184
贾淼 <suzhou136@nahuasuan.com>
2016-11-27 09:06:53 +0800

    新增JCNetwork 主体类

---
+++ JCNetwork/Classes/JCNetworkResponse.m
@@ -0,0 +1,19 @@
+//
+//  JCNetworkResponse.m
+//  JCNetworkNew
+//
+//  Created by Jam on 13-6-23.
+//  Copyright (c) 2013年 Jam. All rights reserved.
+//
+
+#import "JCNetworkResponse.h"
+
+@implementation JCNetworkResponse
+
+@synthesize requestID;
+@synthesize status;
+@synthesize progress;
+@synthesize content;
+@synthesize error;
+
+@end
